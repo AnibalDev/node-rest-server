555bde7c11182b67083411fa5ca39a79a55fa091
Anibal Rodriguez <anibalrodriguezdeveloper@gmail.com>
2023-03-10 10:26:59 -0600

    Test ssh

--- routes/auth.js
+++ routes/auth.js
@@ -9,7 +9,6 @@ router.post('/login',[
     check('email', 'El email es obligatorio.').isEmail(),
     check('password', 'La contraseña es obligatoria.').not().isEmpty(),
     validateFields
-
 ],login);
 
 module.exports = router;
