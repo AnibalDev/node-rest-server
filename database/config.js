3f687159bccc1280e24cc0fecdeb701f7a4027fb
Anibal Rodriguez <anibalrodriguezdeveloper@gmail.com>
2023-03-09 18:39:55 -0600

    remove .env

--- database/config.js
+++ database/config.js
@@ -2,14 +2,12 @@ const mongoose = require('mongoose');
 
 const dbConnection = async() => {
     try {
-        console.log(process.env.MONGODB_ATLAS);
         await mongoose.connect(process.env.MONGODB_ATLAS, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+            //useNewUrlParser: true,
+            //useUnifiedTopology: true,
             //useCreateIndex: true,
             //useFindAndModify: false
         })
-
         console.log('Connected')
         
     } catch (error) {
